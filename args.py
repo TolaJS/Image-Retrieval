6e2e4c7f5c929fe42f384138f967da54037304d2
Tola Shobande <58945003+TolaJS@users.noreply.github.com>
2024-10-11 06:19:58 +0100

    Added argument variables

--- args.py
+++ args.py
@@ -4,3 +4,15 @@ name:           args.py
 date:           30/09/2024
 description:
 """
+
+dropout = 0.1                                           # Dropout Rate
+batch_size = 64                                         # Batch Size
+seed = 7                                                # Chosen seed for reproducibility
+lr = 0.01                                               # Learning Rate
+gamma = 0.95                                            # Gamma rate
+epochs = 10                                             # Number of Epochs
+num_classes = 11                                        # Number of Classes
+weight_decay = 0.0005                                   # Weight Decay
+eval_mode = True                                        # Boolean: True=Evaluating, False=Testing
+save_path = "./checkpoint/"                             # Path to save model checkpoints
+model_path = "./checkpoint/Model-lr_0.01_bs_64-E10.pth" # Change this to saved model file path
