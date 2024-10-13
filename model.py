1a18c6ad961211fd50979a896d77c7d70042db30
Tola Shobande <58945003+TolaJS@users.noreply.github.com>
2024-10-14 00:13:13 +0100

    Changes to EmbeddingNet for use of FC layer, and minor changes to model util function

--- model.py
+++ model.py
@@ -8,29 +8,22 @@ description:
 import torch
 import torch.nn as nn
 from torchvision import models
-from utils.utils import get_model_info
-  
+from utils.utils import get_model_and_features
+
 
 class EmbeddingNet(nn.Module):
-    def __init__(self, model:nn.Module, embedding_dim=128, freeze_weights=True):
+    def __init__(self, model: nn.Module, embedding_dim=None, freeze_weights=True):
         super(EmbeddingNet, self).__init__()
         self.model = model
-        
+
         if freeze_weights:
             for param in self.model.parameters():
                 param.requires_grad = False
 
-        self.model, num_features = get_model_info(self.model)
-        self.avgpool = nn.AdaptiveAvgPool2d((1,1))
-        self.fc = nn.Linear(num_features, embedding_dim)
+        self.model, out_features = get_model_and_features(self.model)
+        if embedding_dim is not None:   # To experiment with different dimensional feature vectors
+            self.model.fc = nn.Linear(out_features, embedding_dim)
     
-    def forward(self,x):
+    def forward(self, x):
         x = self.model(x)
-        if isinstance(x, tuple):
-            x = x[0]
-            
-        x = self.avgpool(x)
-        x = x.view(x.size(0), -1)
-        x = self.fc(x)
-        
         return x   
--- utils/utils.py
+++ utils/utils.py
@@ -23,39 +23,35 @@ def set_seed(seed_value):
     # print(f"Random seed set to: {seed_value}")
 
 
-def get_model_info(model:torch.nn.Module):
-    if isinstance(model, models.ResNet):
-        num_features = model.fc.in_features
+def get_model_and_features(model: torch.nn.Module):
+    if isinstance(model, (models.ResNet, models.GoogLeNet, models.Inception3)):
+        out_features = model.fc.in_features
         model.fc = torch.nn.Identity()
-        
-    if isinstance(model, models.GoogLeNet):
-        num_features = model.fc.in_features
-        model.fc = torch.nn.Identity()
-        
+
     elif isinstance(model, models.EfficientNet):
-        num_features = model.classifier[1].in_features
+        out_features = model.classifier[1].in_features
         model.classifier = torch.nn.Identity()
-        
+
     else:
         raise ValueError(
             f"Unsupported model type: Model is meant for torchvison's "
             f"GoogLeNet, Inception3, ResNet or EfficientNet")
-    
-    return model, num_features
+
+    return model, out_features
 
 
 def format_log_message(i, epoch, train_loss, val_loss):
     """Log formatting function."""
-    return (f'| Epoch:{epoch+1}/{args.epochs} | Train_Loss:{train_loss:8.3f} | '
+    return (f'| Epoch:{epoch + 1}/{args.epochs} | Train_Loss:{train_loss:8.3f} | '
             f'Val_Loss:{val_loss:8.3f}')
 
 
 def print_summary(logger, name, train_size, val_size, test_size):
     """Print summary model and data statistics."""
-    logger.info('*'*17 + 'Summary' + '*'*17)
+    logger.info('*' * 17 + 'Summary' + '*' * 17)
     logger.info(f'# Model: {name}')
     logger.info(f'# Train size: {train_size}')
     logger.info(f'# Validation size: {val_size}')
     logger.info(f'# Test size: {test_size}')
     logger.info(f'# Epochs: {args.epochs}')
-    logger.info('*'*41+'\n')
+    logger.info('*' * 41 + '\n')
